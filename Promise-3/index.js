2d764d2aa11e32a0c737cc11d1de09602cbc6713
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-05-04 14:49:44 -1000

    promise 3

--- Promise-3/index.js
+++ Promise-3/index.js
@@ -33,11 +33,29 @@ greet('Your name') // Returns a Promise
 .then(str => console.log(str)) // Log the results of uppercaser()
 .catch(err => console.log(err)) // Catches an error
 
+// prints 'HELLO YOUR NAME'
+
 // Challenges: get greet() to fail by passing a non string value
 // What happens? 
+
+greet(1000) // Enter a non string value 
+.then(str => uppercaser(str))  
+.then(str => console.log(str)) 
+.catch(err => console.log(err)) 
+
+// prints 'Name must be a string!' rejection message from greet function
 
 // Challenge: get uppercaser() to fail by passing a non string value
 // What happens? 
 
+uppercaser(1000) // Enter a non string value 
+.then(str => console.log(str)) 
+.catch(err => console.log(err)) 
+
+// prints 'Argument to uppercaser must be string' rejection message from uppercaser function
+
+
 // Challenge: Notice there is only a single .catch() at the end. 
 // Explain the behavior?
+
+// It will throw the first error, no matter which one it is, so you only need one catch.

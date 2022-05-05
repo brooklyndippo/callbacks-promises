76ad70f95cbc4dccc108cf8c34725db8d1958df6
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-05-04 15:10:10 -1000

    promsie 6

--- Promise-6/index.js
+++ Promise-6/index.js
@@ -1,15 +1,46 @@
 function whatToEat(timeOfDay) {
-  // Create a new promise and return it here
-  
+  return new Promise((resolve, reject) => {
+      if (timeOfDay == 'breakfast') {
+        resolve('waffles');
+      } else if (timeOfDay == 'lunch') {
+        resolve('soup & salad')
+      } else if (timeOfDay == 'dinner') {
+        resolve('pizza')
+      } else {
+        reject('That is not a proper meal time.')
+      }
+    });
 }
 
 
 // Call yout function and resolve the promise here! 
 whatToEat('lunch')
+.then(message => console.log(message))
+.catch(err => console.log(err))
+.finally(()=> {
+  console.log('Back to Work!');
+})
 
 
 // If the `timeOfDay` is 'breakfast', 'lunch', or 'dinner' 
 // resolve the promise with your favorite food for that time. 
+whatToEat('breakfast')
+.then(message => console.log(message))
+.catch(err => console.log(err))
+.finally(()=> {
+  console.log('Back to Work!');
+})
+
+whatToEat('dinner')
+.then(message => console.log(message))
+.catch(err => console.log(err))
+.finally(()=> {
+  console.log('Back to Work!');
+})
 
 // If the `timeOfDay` is anything else reject the promise with 
 // the 'message' I'm not hungry right now.  
+
+whatToEat('snack')
+.then(message => console.log(message))
+.catch(err => console.log(err))

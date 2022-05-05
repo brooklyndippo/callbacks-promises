1d5707fdcc03c349a6f83a7ad5664d66957ec220
Brooklyn Dippo <brooklyndippo@gmail.com>
2022-05-04 14:57:46 -1000

    promise 4

--- Promise-4/index.js
+++ Promise-4/index.js
@@ -1,11 +1,24 @@
 const food = new Promise((resolve, reject) => {
-  
-})
+  setTimeout(()=> {
+      resolve('Nav Ratan Korma');
+      // reject('Would you like something else instead?')
+  }, 2000);
+});
 
+food.then((message) => {
+    console.log(message);
+    console.log('YUM! 😀');
+  }).catch((err) => {
+    console.log('Sorry, all out. 😞');
+    console.log(err);
+  });
+  
 
 // resolve food here: 
 
 
+
+// chicken
 
 // **Challenge 1** Resolve your promise with the name of your favorite food. 
 // Do this by calling the `resolve` with an argument. 
